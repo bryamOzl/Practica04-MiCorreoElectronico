31052353918f73842c066edd9adbb329877f2892
bryamOzl <bryan95_19@hotmail.com>
2019-11-26 09:56:15 -0500

    Practica04-MiCorreoElectronico

--- public/controladores/login.php
+++ public/controladores/login.php
@@ -3,18 +3,29 @@
     include '../../config/conexionBD.php';
     $usuario = isset($_POST["correo"]) ? trim($_POST["correo"]) : null;
     $contrasena = isset($_POST["contrasena"]) ? trim($_POST["contrasena"]) : null;
-    $sql = "SELECT * FROM usuario WHERE usu_correo = '$usuario' and usu_password = MD5('$contrasena') and usu_rol = 'admin'";
-    $result = $conn->query($sql);
+    $sql = "SELECT * FROM usuario WHERE usu_correo = '$usuario' and usu_password = MD5('$contrasena')";
     //$rol = isset($_POST["rol"]) ? trim($_POST["rol"]) :null;
     //if (){}
     echo $result;
-    /*$sql = "SELECT * FROM usuario WHERE usu_correo = '$usuario' and usu_password = MD5('$contrasena') and usu_rol = 'admin'";
+    $sql = "SELECT * FROM usuario WHERE usu_correo = '$usuario' and usu_password = MD5('$contrasena') and usu_rol = 'admin'";
     $result = $conn->query($sql);
-    if ($result->num_rows > 0) {
+    if ($result->num_rows > 0 ) {
+        while ($row = $result->fetch_assoc()) {
+            $_SESSION['cod'] = "$row[usu_codigo]";
+            $_SESSION['nom'] = "$row[usu_nombres]";
+            $_SESSION['ape'] = "$row[usu_apellidos]";
+            $_SESSION['el'] = "$row[usu_eliminado]";
+            $_SESSION['cor'] = "$usuario";
+            $_SESSION['rol'] = "$row[usu_rol]";
+        }
         $_SESSION['isLogged'] = TRUE;
-        header("Location: ../../admin/vista/admin/index.php");
-    } else {
+        if(){
+            header("Location: ../../admin/vista/admin/index.php");
+        }else {
+            header("Location: ../../admin/vista/user/index.php");
+        }  
+    }else {
         header("Location: ../vista/login.html");
     }
-    $conn->close();*/
+    $conn->close();
 ?>
