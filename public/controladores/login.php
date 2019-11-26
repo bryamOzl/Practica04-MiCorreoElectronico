9b4b8e5cac41576a337591bdfb4fed085555b81a
bryamOzl <bryan95_19@hotmail.com>
2019-11-26 09:19:50 -0500

    Practica04-MiCorreoElectronico

--- public/controladores/login.php
+++ public/controladores/login.php
@@ -5,11 +5,16 @@
     $contrasena = isset($_POST["contrasena"]) ? trim($_POST["contrasena"]) : null;
     $sql = "SELECT * FROM usuario WHERE usu_correo = '$usuario' and usu_password = MD5('$contrasena') and usu_rol = 'admin'";
     $result = $conn->query($sql);
+    //$rol = isset($_POST["rol"]) ? trim($_POST["rol"]) :null;
+    //if (){}
+    echo $result;
+    /*$sql = "SELECT * FROM usuario WHERE usu_correo = '$usuario' and usu_password = MD5('$contrasena') and usu_rol = 'admin'";
+    $result = $conn->query($sql);
     if ($result->num_rows > 0) {
         $_SESSION['isLogged'] = TRUE;
         header("Location: ../../admin/vista/admin/index.php");
     } else {
         header("Location: ../vista/login.html");
     }
-    $conn->close();
+    $conn->close();*/
 ?>
