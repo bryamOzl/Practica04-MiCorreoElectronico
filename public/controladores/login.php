8930752988d1230cb9e0d2abe38bc858b6a48a87
bryamOzl <bryan95_19@hotmail.com>
2019-11-26 10:54:34 -0500

    Practica04-MiCorreoElectronico

--- public/controladores/login.php
+++ public/controladores/login.php
@@ -3,7 +3,7 @@
     include '../../config/conexionBD.php';
     $usuario = isset($_POST["correo"]) ? trim($_POST["correo"]) : null;
     $contrasena = isset($_POST["contrasena"]) ? trim($_POST["contrasena"]) : null;
-    $sql = "SELECT * FROM usuario WHERE usu_correo = '$usuario' and usu_password = MD5('$contrasena')";
+    $sql = "SELECT * FROM usuario WHERE usu_correo = '$usuario' and usu_password = MD5('$contrasena') and usu_eliminado='n'";
     $result = $conn->query($sql);
     if ($result->num_rows > 0 ) {
         while ($row = $result->fetch_assoc()) {
@@ -17,7 +17,7 @@
         $_SESSION['isLogged'] = TRUE;
         if($_SESSION['rol'] == "ADMIN"|| $_SESSION['rol'] == "admin"){
             header("Location: ../../admin/vista/admin/index.php");
-        }else if ($_SESSION['rol'] == "USER" || $_SESSION['rol'] == user){
+        }else if ($_SESSION['rol'] == "USER" || $_SESSION['rol'] == "user"){
             header("Location: ../../admin/vista/user/index.php");
         }  
     }else {
